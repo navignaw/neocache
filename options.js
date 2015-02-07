859e55947a0247b827095d42d9c8c9c970af7cd8
ewang <ewang@ewang-350V5C.(none)>
2015-02-07 15:21:52 -0500

    so much fail async stupid options page

--- options.js
+++ options.js
@@ -11,17 +11,49 @@
 
   function displayList(curUser) {
     var Payload = Parse.Object.extend("Payload");
-    var userQuery = new Parse.Query(Payload);
-    userQuery.equalTo("user", curUser);
-    userQuery.find().then(function(results) {
+    var payloadQuery = new Parse.Query(Payload);
+    payloadQuery.equalTo("user", curUser);
+    payloadQuery.include("page");
+    payloadQuery.find().then(function(results) {
       var list = document.getElementById("payloads");
       for (var i = 0; i < results.length; i++) {
         var res = results[i];
-        console.log(res);
         var li = document.createElement("li");
-        var resStr = res.get("page").get("url") + ":" + res.content;
+        var resStr = res.get("page").get("url") + " : \t" + res.get("content");
+        console.log(resStr);
         li.innerHTML = resStr;
+        list.insertBefore(li, list.firstChild);
       }
     });
   }
+
+  /*
+      var promise = Parse.Promise.as();
+      var payload = [];
+      for (var i = 0; i < results.length; i++) {
+        
+      //_.each(results, function(result) {
+        promise = promise.then(function(payloads) {
+          // var pageQuery = Parse.Object.extend("Page");
+          // pageQuery.equalTo(result.get("page").id)
+          payloads.push(
+          return results[i].get("page").get("url");
+        });
+      }
+      return promise;
+      /* var list = document.getElementById("payloads");
+      for (var i = 0; i < results.length; i++) {
+        var res = results[i];
+        var li = document.createElement("li");
+        var resStr = res.get("page").get("url") + ":" + res.get("content");
+        console.log(res);
+        console.log(resStr);
+        li.innerHTML = resStr;
+      } */
+  /*  }).then(function(promise) {
+      var li = document.createElement("li");
+      console.log(promise);
+      li.innerHTML =  promise + ":" + results[i].get("content"));
+    });
+  }*/
 })();
