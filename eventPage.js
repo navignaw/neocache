759c4c74a107562d92ed69e6538fb76fa26180eb
Ivan Wang <icw@cmu.edu>
2015-02-07 02:18:27 -0500

    Retrieve payloads from current url from Parse

--- eventPage.js
+++ eventPage.js
@@ -1,7 +1,18 @@
 /* Script runs once in background */
 
-chrome.runtime.onStartup.addListener(function() {
+/*chrome.runtime.onStartup.addListener(function() {
   console.log("initializing Parse");
   Parse.initialize("Qh5UNx6rO5d9RPKHkW7OAAgabKGlbcByuaWZXdFB", "NcwUMIqVFnmYHjzkrlRFIPWaKhytP0kIhwLKdpBa");
-});
+});*/
 
+// Load all payloads on tab creation/update
+chrome.tabs.onCreated.addListener(function(tab) {
+  if (tab.active) {
+    chrome.tabs.sendMessage(tabId, {url: tab.url});
+  }
+});
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status === 'complete' && tab.active) {
+    chrome.tabs.sendMessage(tabId, {url: tab.url});
+  }
+});
--- payloader.js
+++ payloader.js
@@ -1,6 +1,35 @@
 /* Content script for loading payloads */
 
-$(document).ready(function() {
+(function() {
 
-});
+  Parse.initialize("Qh5UNx6rO5d9RPKHkW7OAAgabKGlbcByuaWZXdFB", "NcwUMIqVFnmYHjzkrlRFIPWaKhytP0kIhwLKdpBa");
 
+  var payloads;
+
+  // Load all payloads on tab update
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.url) {
+      var newUrl = request.url.split("?")[0]; // remove url parameters
+      console.log('Searching for payloads on ' + newUrl);
+
+      var Page = Parse.Object.extend("Page");
+      var pageQuery = new Parse.Query(Page);
+      pageQuery.equalTo("url", newUrl);
+      pageQuery.find().then(function(results) {
+        if (results.length > 0) {
+          var page = results[0];
+          var Payload = Parse.Object.extend("Payload");
+          var payloadQuery = new Parse.Query(Payload);
+          payloadQuery.equalTo("page", page);
+          return payloadQuery.find();
+        }
+        return Parse.Promise.as([]);
+
+      }).then(function(results) {
+        payloads = results;
+        console.log(payloads.length + " payloads found");
+        console.log(payloads);
+      });
+    }
+  });
+})();
